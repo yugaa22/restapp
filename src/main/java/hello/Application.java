acd36e0e1ecaa275f6ee4863e1506a0c73a7ae1e
kiran <kiran@opsmx.com>
2016-08-02 15:27:39 +0530

    Print Beans provided by SpringBoot

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,7 +1,11 @@
 package hello;
 
+
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 
 @SpringBootApplication
@@ -9,6 +13,14 @@ public class Application {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    	ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
     }
 }
