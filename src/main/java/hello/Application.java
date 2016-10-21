d3d54326c87ba77245ac05e94b439145eb2a598a
kiran <crackerplace@gmail.com>
2016-10-21 13:52:04 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -6,10 +6,34 @@ import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean; 
+
+import org.springframework.jmx.support.ConnectorServerFactoryBean; 
+import org.springframework.remoting.rmi.RmiRegistryFactoryBean;
 
 @SpringBootApplication
 public class Application {
-
+    @Value("${jmx.rmi.host:localhost}")
+    private String rmiHost;
+    @Value("${jmx.rmi.port:1092}")
+    private Integer rmiPort;
+ 
+    @Bean
+    public RmiRegistryFactoryBean rmiRegistry() {
+        final RmiRegistryFactoryBean rmiRegistryFactoryBean = new RmiRegistryFactoryBean();
+        rmiRegistryFactoryBean.setPort(rmiPort);
+        rmiRegistryFactoryBean.setAlwaysCreate(true);
+        return rmiRegistryFactoryBean;
+    }
+ 
+    @Bean
+    @DependsOn("rmiRegistry")
+    public ConnectorServerFactoryBean connectorServerFactoryBean() throws Exception {
+        final ConnectorServerFactoryBean connectorServerFactoryBean = new ConnectorServerFactoryBean();
+        connectorServerFactoryBean.setObjectName("connector:name=rmi");
+        connectorServerFactoryBean.setServiceUrl(String.format("service:jmx:rmi://%s:%s/jndi/rmi://%s:%s/jmxrmi", rmiHost, rmiPort, rmiHost, rmiPort));
+        return connectorServerFactoryBean;
+    }
 
     public static void main(String[] args) {
     	ApplicationContext ctx = SpringApplication.run(Application.class, args);
