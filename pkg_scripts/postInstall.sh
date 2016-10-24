308e1c27f798ee42bf0a2c6a42f440bdcadd900b
kiran <crackerplace@gmail.com>
2016-10-24 14:23:16 +0530

    Update postInstall.sh

--- pkg_scripts/postInstall.sh
+++ pkg_scripts/postInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+cd /opt/stackdriver/collectd/etc/collectd.d/ && curl -O https://raw.githubusercontent.com/OpsMx/server_monitor/master/tomcat-7.conf
 ln -s /opt/restapp/restapp-0.1.0.jar /etc/init.d/restapp
 ##/etc/init.d/restapp start
 update-rc.d restapp defaults
