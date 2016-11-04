8be52c37159a9130e3de05029eab199a6faf06c2
OpsMx <accounts@opsmx.com>
2016-11-04 18:29:14 +0530

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -18,10 +18,10 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
     public static Map<BadKey,String> leakMap = new HashMap<>();
     
-    
+   
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-         for(int i =0;i<10;i++){
+         for(int i =0;i<13;i++){
            leakMap.put(new BadKey("key"), "value");
          }
          System.out.println("HashMap size : "+ leakMap.size());
