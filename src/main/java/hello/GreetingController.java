849abb942ed860d93eeae2a5dec9d21e1904aeda
madhu <madhukarr@ipsgsystems.com>
2016-11-08 12:44:12 +0530

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -20,7 +20,8 @@ public class GreetingController {
    
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        for(int i =0;i<13;i++){
+        //memory leak code
+        for(int i =0;i<12;i++){
            leakMap.put(new BadKey("key"), "value");
          }                
          System.out.println("HashMap size : "+ leakMap.size());
