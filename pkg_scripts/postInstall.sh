2d5af14106cd4348a931cbd8843f295c460b73ce
OpsMx <accounts@opsmx.com>
2016-11-09 15:49:35 +0530

    Update postInstall.sh

--- pkg_scripts/postInstall.sh
+++ pkg_scripts/postInstall.sh
@@ -3,8 +3,11 @@
 ln -s /opt/restapp/restapp-0.1.0.jar /etc/init.d/restapp
 ##/etc/init.d/restapp start
 update-rc.d restapp defaults
+sleep 60
 apt-get install -y wget curl
+sleep 60
 curl -O "https://repo.stackdriver.com/stack-install.sh"
+sleep 60
 bash stack-install.sh --write-gcm
 cd /opt/stackdriver/collectd/etc/collectd.d/
 sudo curl -O https://raw.githubusercontent.com/OpsMx/server_monitor/master/tomcat-7.conf
