27ed2a618d7893ff1743a8010190916f77b50ead
OpsMx <accounts@opsmx.com>
2016-11-11 14:39:48 +0530

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -18,6 +18,7 @@ public class GreetingController {
     public static Map<BadKey,String> leakMap = new HashMap<>();
     
    
+    
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         //memory leak code
@@ -33,6 +34,7 @@ public class GreetingController {
     @RequestMapping(value = "/health")
     public void health() {
     }
+    
     
     @RequestMapping(value = "/mbeans")
     public void mbeans() throws Exception {
