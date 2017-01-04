10556c7c5fdfd5854ddab6c49059a5dffb4b139c
madhu <madhukarr@ipsgsystems.com>
2017-01-04 16:56:10 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,8 +1,6 @@
 package hello;
 
-
 import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
