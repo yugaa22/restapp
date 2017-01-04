8b1fa7648d1e72d867460ecb642f7b827cf9fc66
madhu <madhukarr@ipsgsystems.com>
2017-01-04 18:51:56 +0530

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,7 +1,5 @@
 package hello;
 
-
-
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
