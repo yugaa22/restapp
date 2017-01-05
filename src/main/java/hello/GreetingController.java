20264e1c2d38370030ea4c61361e9ff4a73c2457
madhu <madhukarr@ipsgsystems.com>
2017-01-05 15:43:08 +0530

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -26,7 +26,6 @@ public class GreetingController {
            leakMap.put(new BadKey("key"), "value");
          }  */ 
         
-        
          System.out.println("HashMap size : "+ leakMap.size());
         return new Greeting(MetricsRegistryClient.client().incrRequestCount(),
                             String.format(template, name));
