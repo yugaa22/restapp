84b16e58bf8520db5983740cc0da3fe560316641
madhu <madhukarr@ipsgsystems.com>
2017-02-13 16:01:41 +0530

    Update postInstall.sh

--- pkg_scripts/postInstall.sh
+++ pkg_scripts/postInstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ln -s -Dserver.tomcat.max-threads=10 /opt/restapp/restapp-0.1.0.jar /etc/init.d/restapp
+ln -s /opt/restapp/restapp-0.1.0.jar /etc/init.d/restapp
 ##/etc/init.d/restapp start
 update-rc.d restapp defaults
