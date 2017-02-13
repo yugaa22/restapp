59cd91d709ffd28a57629a7a1d4106136b617314
madhu <madhukarr@ipsgsystems.com>
2017-02-13 16:48:42 +0530

    Update GreetingControllerTests.java

--- src/test/java/hello/GreetingControllerTests.java
+++ src/test/java/hello/GreetingControllerTests.java
@@ -32,9 +32,17 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
 /**
  * @author Greg Turnquist
  */
+
+@Configuration
+@PropertySource("file:src/main/resources/application.properties")
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
