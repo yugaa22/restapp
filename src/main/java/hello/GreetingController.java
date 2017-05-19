b25a43e61f802ee3ee6216ecb8cdfee6dffaed16
madhu <madhukarr@ipsgsystems.com>
2017-05-19 15:48:49 +0530

    With memory leak

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -56,7 +56,7 @@ public class GreetingController {
 	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 
 		// demonstrating memory leak
-	/*	if (MEMORY_LEAK_TEST_STRING == null || MEMORY_LEAK_TEST_STRING.length() == 0) {
+		if (MEMORY_LEAK_TEST_STRING == null || MEMORY_LEAK_TEST_STRING.length() == 0) {
 			for (int i = 0; i < 30000; i++) {
 				String test = "" + i + "" + i + "" + i;
 				MEMORY_LEAK_TEST_STRING += test;
@@ -69,12 +69,12 @@ public class GreetingController {
 		BadKey badKey = new BadKey("");
 		badKey = new BadKey("" + (new Date().getTime()));
 		BAD_KEY_MAP.put(badKey,	"" + (new Date().getTime()) + "-" + (new Date().getTime()) + MEMORY_LEAK_TEST_STRING);		
-	*/	
+		
 		// demonstrating architectural regression
-	//	POSTGRES_NUM_OPS_METRIC_COUNT += 1;
+		//POSTGRES_NUM_OPS_METRIC_COUNT += 1;
 		//for (int i = 0; i < 6; i++) {
 		//	POSTGRES_NUM_OPS_METRIC_COUNT += (i + 1);
-	//		getAllUsersFromDB();
+		//	getAllUsersFromDB();
 		//}
 
 /*		if (Long.compare(TIMESTAMP, 0l) == 0
