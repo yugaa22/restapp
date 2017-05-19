45652d9f11436506222637e91fa844cc12b0be45
madhu <madhukarr@ipsgsystems.com>
2017-05-19 09:13:48 -0700

    Update GreetingController.java
    Fix Memleak problem

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -68,7 +68,7 @@ public class GreetingController {
 
 		BadKey badKey = new BadKey("");
 		badKey = new BadKey("" + (new Date().getTime()));
-		BAD_KEY_MAP.put(badKey,	"" + (new Date().getTime()) + "-" + (new Date().getTime()) + MEMORY_LEAK_TEST_STRING);		
+		//BAD_KEY_MAP.put(badKey,	"" + (new Date().getTime()) + "-" + (new Date().getTime()) + MEMORY_LEAK_TEST_STRING);		
 		
 		// demonstrating architectural regression
 		//POSTGRES_NUM_OPS_METRIC_COUNT += 1;
