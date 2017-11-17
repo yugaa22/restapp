ab8d6d26573db4e0dda2b317758b135d8787d874
phani <phanikumarp@users.noreply.github.com>
2017-11-17 16:29:24 +0530

    Update dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-java -Dserver.port=8095 -jar /restapp.ar &
+java -Dserver.port=8095 -jar /restapp.jar &
 hostname="hostname: `hostname`"
 sed -i "s/# hostname: mymachine.mydomain/$hostname/g" /etc/dd-agent/datadog.conf
 service datadog-agent start
