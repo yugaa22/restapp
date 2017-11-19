86fcceb2b7d7bf1802c355cd8d34f7225a246e2e
OpsMx <accounts@opsmx.com>
2017-11-20 00:21:31 +0530

    Update dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -2,6 +2,7 @@
 java -Dserver.port=8095 -jar /restapp.jar &
 hostname="hostname: `hostname`"
 sed -i "s/# hostname: mymachine.mydomain/$hostname/g" /etc/dd-agent/datadog.conf
+nohup stress-ng --cpu 1 --vm 2 &
 service datadog-agent start
 while true; do
   sleep 1000
