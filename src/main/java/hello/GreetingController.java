df01a09051ac0acc1b292ed4b944ef1d7e2cc9c5
j-sandy <30489233+j-sandy@users.noreply.github.com>
2017-12-15 16:29:25 +0530

    Resolved conficted code

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -175,10 +175,6 @@ public class GreetingController {
 		String result="{ \"catCount\": 0 }";
 		/*Code for Architectural Regression, prerequisite is to have restapp running on k8 pod */
 		try{
-			for (int i=0; i<=10; i++)
-	      String result="{ \"catCount\": 0 }";
-		/*Code for Architectural Regression, prerequisite is to have restapp running on k8 pod */
-		try{
 			for (int i=0; i<=1000; i++)
 			{
 				URL url = new URL("http://35.192.98.201:8080/catcount");
