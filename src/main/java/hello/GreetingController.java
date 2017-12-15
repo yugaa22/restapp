079d120d3f61f718191f7d42190f61e12061fda0
j-sandy <jsandy>
2017-12-15 16:25:03 +0530

    Merging changes

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -172,17 +172,14 @@ public class GreetingController {
 	
 	@RequestMapping("/catcount")
 	public String catCount() {
-<<<<<<< HEAD
 		String result="{ \"catCount\": 0 }";
 		/*Code for Architectural Regression, prerequisite is to have restapp running on k8 pod */
 		try{
 			for (int i=0; i<=10; i++)
-=======
 	      String result="{ \"catCount\": 0 }";
 		/*Code for Architectural Regression, prerequisite is to have restapp running on k8 pod */
 		try{
 			for (int i=0; i<=1000; i++)
->>>>>>> e3ae0b3f258773d3179041ff871b8844c4c6034d
 			{
 				URL url = new URL("http://35.192.98.201:8080/catcount");
 				HttpURLConnection con = (HttpURLConnection)url.openConnection();
