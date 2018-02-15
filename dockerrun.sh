34447a4f11a036ae771ee139cfb50ad3086770b7
Anush S <anush.sekharamantri@gmail.com>
2018-02-15 13:35:00 +0530

    Container containing new-relic jar instead of Prometheus

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-nohup java -javaagent:./jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml -jar restapp.jar &
-#java -Dserver.port=8095 -jar /restapp.jar &
-#hostname="hostname: `hostname`"
-#nohup stress-ng --vm 4 &
-#sed -i "s/# hostname: mymachine.mydomain/$hostname/g" /etc/dd-agent/datadog.conf
-#service datadog-agent start
+nohup java -javaagent:/opt/newrelic/newrelic.jar -jar restapp.jar &
 while true; do
   sleep 1000
 done
