f7c5f860a974f2ea3bfcaf11e2d56414448cd4eb
sher4locked <33213210+sher4locked@users.noreply.github.com>
2018-03-12 15:48:45 +0530

    Removing unnecessary Prometheus agent

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-nohup java -javaagent:/opt/newrelic/newrelic.jar -javaagent:/opt/prometheus/jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml -jar restapp.jar &
+nohup java -javaagent:/opt/newrelic/newrelic.jar -jar restapp.jar &
 while true; do
   sleep 1000
 done
