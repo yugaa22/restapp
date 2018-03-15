38c759b2090a32e3e20ae6ad735bfb55f4224ab2
sher4locked <33213210+sher4locked@users.noreply.github.com>
2018-03-15 14:26:05 +0530

    Much lesser sleep (50ms)

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -128,7 +128,7 @@ public class GreetingController {
 			br.close();
 			
 			  String whichdog = "dog1";
-			  Thread.sleep(1000);
+			  Thread.sleep(50);
 			
 			/* Introducing Arch. regression*/
 			      //if(10.0*Math.random()>5.0)
