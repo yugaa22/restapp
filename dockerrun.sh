9c3e3e7b25e7d92da168340a8660b009674a438c
phani <phanikumarp@users.noreply.github.com>
2018-06-20 11:53:22 +0530

    Update dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-nohup java -javaagent:./jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml -jar restapp.jar > /var/log/rest_service.log 2>&1 &
-#nohup java -Dserver.port=8095 -jar /restapp.jar  > /var/log/rest_service.log 2>&1 &
+nohup java -javaagent:/opt/jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &
+#nohup java -Dserver.port=8095 -jar /opt/restapp.jar  > /var/log/rest_service.log 2>&1 &
 #hostname="hostname: `hostname`"
 #nohup stress-ng --vm 4 &
 #sed -i "s/# hostname: mymachine.mydomain/$hostname/g" /etc/dd-agent/datadog.conf
