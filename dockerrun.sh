738528e99026e00140f886444abfedb06772aff7
phani <phanikumarp@users.noreply.github.com>
2018-06-20 10:53:25 +0530

    removed monitering

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
-nohup java -javaagent:./jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml -jar restapp.jar &
-#java -Dserver.port=8095 -jar /restapp.jar &
+#!/bin/bash
+#nohup java -javaagent:./jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml -jar restapp.jar > /var/log/rest_service.log 2>&1 &
+nohup java -Dserver.port=8095 -jar /restapp.jar  > /var/log/rest_service.log 2>&1 &
 #hostname="hostname: `hostname`"
 #nohup stress-ng --vm 4 &
 #sed -i "s/# hostname: mymachine.mydomain/$hostname/g" /etc/dd-agent/datadog.conf
