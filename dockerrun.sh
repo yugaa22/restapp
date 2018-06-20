cb8c2e95b02f26e116f67b01e3fe50bec360d066
phani <phanikumarp@users.noreply.github.com>
2018-06-20 13:57:58 +0530

    server port updated

--- dockerrun.sh
+++ dockerrun.sh
@@ -6,7 +6,7 @@
 #nohup java -javaagent:/opt/jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &
 
 ##====> for newrelic
-(cd /opt;nohup java  -Dserver.port=8090 -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_new_service.log 2>&1 &)
+(cd /opt;nohup java  -Dserver.port=${server_port} -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_new_service.log 2>&1 &)
 
 #====> for datadog
 #hostname="hostname: `hostname`"
