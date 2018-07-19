605ab40a5d6877fd6c085eb3cfa999e445230cc4
phani <phanikumarp@users.noreply.github.com>
2018-07-19 19:58:58 +0530

    Enabling logs for testing

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##====> for simple restapp
 (cd /opt;nohup java -Dserver.port=8080 -jar /opt/restapp.jar  > /var/log/rest_service.log 2>&1 &)
-## tail -f /var/log/rest_service.log &
+tail -f /var/log/rest_service.log &
 
 ##====> for promethues
 #nohup java -javaagent:/opt/jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &
