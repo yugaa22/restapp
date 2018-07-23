cdc196998545445988ab4055744c2b3df444b94a
phani <phanikumarp@users.noreply.github.com>
2018-07-23 15:03:25 +0530

    Update dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ##====> for simple restapp
 (cd /opt;nohup java -Dserver.port=8080 -jar /opt/restapp.jar  > /var/log/rest_service.log 2>&1 &)
-tail -f /var/log/rest_service.log &
+sudo chmod 775 /var/log/rest_service.log
+sudo tail -f /var/log/rest_service.log &
 
 ##====> for promethues
 #nohup java -javaagent:/opt/jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &
