110c947befdc244ed383e6e3df1de85288810f20
phani <phanikumarp@users.noreply.github.com>
2018-07-27 14:57:39 +0530

    added promethues agent

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 ##====> for simple restapp
-(cd /opt;nohup java -Dserver.port=8080 -jar /opt/restapp.jar  > /var/log/rest_service.log 2>&1 &)
+#(cd /opt;nohup java -Dserver.port=8080 -jar /opt/restapp.jar  > /var/log/rest_service.log 2>&1 &)
+
+
+##====> for promethues
+(cd /opt;nohup java -javaagent:/opt/jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
 sudo chmod 775 /var/log/rest_service.log
 sudo tail -f /var/log/rest_service.log &
 
-##====> for promethues
-#nohup java -javaagent:/opt/jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &
 
 ##====> for newrelic
 #(cd /opt;nohup java  -Dserver.port=${server_port} -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
