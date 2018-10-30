6531d39d3ec438f378bc895e2a4f4bf7f4ae92cb
phani <phanikumarp@users.noreply.github.com>
2018-10-30 19:55:50 +0530

    Important! Changing port to 8083

--- dockerrun.sh
+++ dockerrun.sh
@@ -10,7 +10,7 @@
 
 
 ##====> for newrelic
-(cd /opt;nohup java  -Dserver.port=${server_port} -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
+(cd /opt;nohup java  -Dserver.port=8083 -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
 sudo chmod 775 /var/log/rest_service.log
 sudo tail -f /var/log/rest_service.log &
 
