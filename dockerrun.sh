bdc0a032ae2af0ab59b1dab5706ea54aa60e2be2
phani <phanikumarp@users.noreply.github.com>
2018-12-05 11:14:59 +0530

    Update dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ##====> for simple restapp
-(cd /opt;nohup java -Dserver.port=8080 -jar /opt/restapp.jar  > /var/log/rest_service.log 2>&1 &)
+#(cd /opt;nohup java -Dserver.port=8080 -jar /opt/restapp.jar  > /var/log/rest_service.log 2>&1 &)
 
 ##====> for promethues
 #nohup java -javaagent:/opt/jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &
 
 ##====> for newrelic
-#(cd /opt;nohup java  -Dserver.port=${server_port} -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_new_service.log 2>&1 &)
+(cd /opt;nohup java  -Dserver.port=${server_port} -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_new_service.log 2>&1 &)
 
 #====> for datadog
 #hostname="hostname: `hostname`"
