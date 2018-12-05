726287fb45e39e14b759755ef19a1edf4f8c18c6
phani <phanikumarp@users.noreply.github.com>
2018-12-05 12:17:35 +0530

    test branch

--- dockerrun.sh
+++ dockerrun.sh
@@ -6,7 +6,7 @@
 #nohup java -javaagent:/opt/jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &
 
 ##====> for newrelic
-(cd /opt;nohup java  -Dserver.port=${server_port} -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_new_service.log 2>&1 &)
+(cd /opt;nohup java  -Dserver.port=8080 -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_new_service.log 2>&1 &)
 
 #====> for datadog
 #hostname="hostname: `hostname`"
@@ -14,5 +14,5 @@
 #sed -i "s/# hostname: mymachine.mydomain/$hostname/g" /etc/dd-agent/datadog.conf
 #service datadog-agent start
 while true; do
-  sleep 1000
+  sleep 100
 done
