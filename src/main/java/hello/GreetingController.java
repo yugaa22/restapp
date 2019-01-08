c406124896942bdc0b5bff404cfa50d4dc5a926d
phani <phanikumarp@users.noreply.github.com>
2019-01-08 16:57:37 +0530

    Update GreetingController.java

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -201,7 +201,7 @@ public class GreetingController {
 		long startTime = System.nanoTime();
 
 		LOG.debug("BEGIN: dogCount");
-		String response = "{ \"dogCount is \": 22 }";
+		String response = "{ \"dogCount is \": 04 }";
 		
 		sim.simulate();
 		
@@ -219,7 +219,7 @@ public class GreetingController {
 		long startTime = System.nanoTime();
 
 		LOG.debug("BEGIN: catCount");
-		String result="{ \"catCount\": 1 }";
+		String result="{ \"catCount\": 01 }";
 		/*Code for Architectural Regression, prerequisite is to have restapp running on k8 pod */
 		/*try{
 			for (int i=0; i<=1000; i++)
