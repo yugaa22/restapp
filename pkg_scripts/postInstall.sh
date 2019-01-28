e7df75b1ece8c4ad083a7b17f8f150f4019a1abc
phani <phanikumarp@users.noreply.github.com>
2019-01-28 23:25:34 +0530

    Update postInstall.sh

--- pkg_scripts/postInstall.sh
+++ pkg_scripts/postInstall.sh
@@ -5,6 +5,7 @@ ln -s /opt/restapp/restapp-0.1.0.jar /etc/init.d/restapp
 update-rc.d restapp defaults
 ## PROMETHEUS AGENT TEST
 sudo /etc/init.d/restapp stop
-sudo wget -qO /opt/jmx_prometheus_javaagent-0.1.0.jar https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.1.0/jmx_prometheus_javaagent-0.1.0.jar
-sudo wget -qO /opt/tomcat.yml https://raw.githubusercontent.com/OpsMx/restapp/master/prometheus/tomcat.yml
-sudo nohup java -javaagent:/opt/jmx_prometheus_javaagent-0.1.0.jar=9090:/opt/tomcat.yml -jar /opt/restapp/restapp-0.1.0.jar &
+#sudo wget -qO /opt/jmx_prometheus_javaagent-0.1.0.jar https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.1.0/jmx_prometheus_javaagent-0.1.0.jar
+#sudo wget -qO /opt/tomcat.yml https://raw.githubusercontent.com/OpsMx/restapp/master/prometheus/tomcat.yml
+#sudo nohup java -javaagent:/opt/jmx_prometheus_javaagent-0.1.0.jar=9090:/opt/tomcat.yml -jar /opt/restapp/restapp-0.1.0.jar &
+sudo nohup java -jar /opt/restapp/restapp-0.1.0.jar &
