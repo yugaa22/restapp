1e2111b91aac81a3608d3bbf403798255754a3f8
phani <phanikumarp@users.noreply.github.com>
2019-02-08 10:33:10 +0530

    Update dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ##====> for simple restapp
-#(cd /opt;nohup java -Dserver.port=8080 -jar /opt/restapp.jar  > /var/log/rest_service.log 2>&1 &)
+cd /opt;nohup java -Dserver.port=8080 -jar /opt/restapp.jar  > /var/log/rest_service.log 2>&1 &
 
 
 ##=====> for promethues
@@ -9,7 +9,7 @@
 #sudo tail -f /var/log/rest_service.log &
 
 ###====> for newrelic
-(cd /opt;nohup java  -Dserver.port=8080  -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
+#(cd /opt;nohup java  -Dserver.port=8080  -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
 sudo chmod 775 /var/log/rest_service.log
 sudo tail -f /var/log/rest_service.log &
 
