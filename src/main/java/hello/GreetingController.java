8ac135ac8460fe662c4bc115e534e20546942512
phani <phanikumarp@users.noreply.github.com>
2019-02-13 23:28:31 +0530

    test12

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -60,7 +60,7 @@ public class GreetingController {
 	public static final String USER = "postgres";
 	public static final String PASS = "networks123";
 	public static String testData = "latency";
-	// System.out.println("Phani test11");
+	// System.out.println("Phani test12");
 
 	@RequestMapping("/greeting")
 	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
