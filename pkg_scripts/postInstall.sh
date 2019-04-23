b51765031fff4b62b0aaf670048eaf887f5170e0
phani <phanikumarp@users.noreply.github.com>
2019-04-23 13:10:41 +0530

    Update postInstall.sh

--- pkg_scripts/postInstall.sh
+++ pkg_scripts/postInstall.sh
@@ -3,9 +3,17 @@
 ln -s /opt/restapp/restapp-0.1.0.jar /etc/init.d/restapp
 ##/etc/init.d/restapp start
 update-rc.d restapp defaults
-## PROMETHEUS AGENT TEST
+###====> for Newrelic monitor
 sudo /etc/init.d/restapp stop
+sudo mkdir -p /opt/newrelic/
+sudo wget -qO /opt/newrelic/newrelic.yml  https://raw.githubusercontent.com/OpsMx/restapp/master/newrelic/newrelic.yml
+sudo wget -qO /opt/newrelic/newrelic.jar https://github.com/OpsMx/restapp/blob/master/newrelic/newrelic.jar?raw=true
+sudo wget -qO /opt/newrelic/newrelic-api.jar  https://github.com/OpsMx/restapp/blob/master/newrelic/newrelic-api.jar?raw=true
+sudo chmod 777 /opt/newrelic/*.jar
+(cd /opt;nohup java  -Dserver.port=8080  -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp-0.1.0.jar > /var/log/rest_service.log 2>&1 &)
+## PROMETHEUS AGENT TEST
+#sudo /etc/init.d/restapp stop
 #sudo wget -qO /opt/jmx_prometheus_javaagent-0.1.0.jar https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.1.0/jmx_prometheus_javaagent-0.1.0.jar
 #sudo wget -qO /opt/tomcat.yml https://raw.githubusercontent.com/OpsMx/restapp/master/prometheus/tomcat.yml
 #sudo nohup java -javaagent:/opt/jmx_prometheus_javaagent-0.1.0.jar=9090:/opt/tomcat.yml -jar /opt/restapp/restapp-0.1.0.jar &
-sudo nohup java -jar /opt/restapp/restapp-0.1.0.jar &
+#sudo nohup java -jar /opt/restapp/restapp-0.1.0.jar &
