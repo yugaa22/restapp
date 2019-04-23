0fe77c9f42a260e64d9164ca83cb56ba139d36fe
phani <phanikumarp@users.noreply.github.com>
2019-04-23 15:10:31 +0530

    Update postInstall.sh

--- pkg_scripts/postInstall.sh
+++ pkg_scripts/postInstall.sh
@@ -10,7 +10,7 @@ sudo wget -qO /opt/newrelic/newrelic.yml  https://raw.githubusercontent.com/OpsM
 sudo wget -qO /opt/newrelic/newrelic.jar https://github.com/OpsMx/restapp/blob/master/newrelic/newrelic.jar?raw=true
 sudo wget -qO /opt/newrelic/newrelic-api.jar  https://github.com/OpsMx/restapp/blob/master/newrelic/newrelic-api.jar?raw=true
 sudo chmod 777 /opt/newrelic/*.jar
-(cd /opt;nohup java  -Dserver.port=8080  -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp-0.1.0.jar > /var/log/rest_service.log 2>&1 &)
+(cd /opt;nohup java -Dserver.port=8080  -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp/restapp-0.1.0.jar &)
 ## PROMETHEUS AGENT TEST
 #sudo /etc/init.d/restapp stop
 #sudo wget -qO /opt/jmx_prometheus_javaagent-0.1.0.jar https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.1.0/jmx_prometheus_javaagent-0.1.0.jar
