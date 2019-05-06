8fbcd5cafc65a3958b3d0f97282d622e4ba8d212
madhu <madhukarr@ipsgsystems.com>
2019-05-06 13:53:04 +0530

    Update dockerrun.sh

--- dockerrun.sh
+++ dockerrun.sh
@@ -4,12 +4,12 @@
 
 
 ##=====> for promethues
-(cd /opt;nohup java -javaagent:/opt/jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
-sudo chmod 775 /var/log/rest_service.log
-sudo tail -f /var/log/rest_service.log &
+#(cd /opt;nohup java -javaagent:/opt/jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
+#sudo chmod 775 /var/log/rest_service.log
+#sudo tail -f /var/log/rest_service.log &
 
 ###====> for newrelic
-#(cd /opt;nohup java  -Dserver.port=8080  -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
+(cd /opt;nohup java  -Dserver.port=8080  -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
 sudo chmod 775 /var/log/rest_service.log
 sudo tail -f /var/log/rest_service.log &
 
