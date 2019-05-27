7ad4ef25342bf2be485ef11299e8ea08bd3b93ad
Madhu <madhukarr@ipsgsystems.com>
2019-05-27 18:28:05 +0530

    Changed ports

--- dockerrun.sh
+++ dockerrun.sh
@@ -9,7 +9,7 @@
 #sudo tail -f /var/log/rest_service.log &
 
 ###====> for newrelic
-(cd /opt;nohup java  -Dserver.port=8081  -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
+(cd /opt;nohup java  -Dserver.port=8080  -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
 sudo chmod 775 /var/log/rest_service.log
 sudo tail -f /var/log/rest_service.log &
 
