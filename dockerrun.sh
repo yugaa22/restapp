9a3bb4b1b1cfb39702345a082bc8274ab1c31e1d
phani <phanikumarp@users.noreply.github.com>
2019-08-05 11:17:41 +0530

    enabled newrelic moniter

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ##====> for simple restapp
-(cd /opt;nohup java  -Dserver.port=8080 -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
-sudo chmod 775 /var/log/rest_service.log
-sudo tail -f /var/log/rest_service.log &
+#(cd /opt;nohup java  -Dserver.port=8080 -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
+#sudo chmod 775 /var/log/rest_service.log
+#sudo tail -f /var/log/rest_service.log &
 
 ##=====> for promethues
 #(cd /opt;nohup java -javaagent:/opt/jmx_prometheus_javaagent-0.1.0.jar=9090:tomcat.yml -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
@@ -10,9 +10,9 @@ sudo tail -f /var/log/rest_service.log &
 #sudo tail -f /var/log/rest_service.log &
 
 ###====> for newrelic
-#(cd /opt;nohup java  -Dserver.port=8080  -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
-#sudo chmod 775 /var/log/rest_service.log
-#sudo tail -f /var/log/rest_service.log &
+(cd /opt;nohup java  -Dserver.port=8080  -javaagent:/opt/newrelic/newrelic.jar -jar /opt/restapp.jar > /var/log/rest_service.log 2>&1 &)
+sudo chmod 775 /var/log/rest_service.log
+sudo tail -f /var/log/rest_service.log &
 
 #====> for datadog
 #hostname="hostname: `hostname`"
