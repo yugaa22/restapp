e9cd9e85496b36f8b1684be87d19b81996cc84f0
Madhu <madhukarr@ipsgsystems.com>
2019-08-13 12:46:23 +0530

    Introduced exceptions for log analysis

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -42,7 +42,7 @@ public class GreetingController {
 	private static final Logger LOG = LoggerFactory.getLogger(GreetingController.class);
 	private static final String KAIROSDB_IP_ADDRESS = "52.8.104.253";
 	private static final Integer PORT = 4343;
-	
+
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 	// public static Map<BadKey,String> leakMap = new HashMap<>();
@@ -50,26 +50,25 @@ public class GreetingController {
 	public static String MEMORY_LEAK_TEST_STRING = "";
 
 	private static final String PREFIX = "put ";
-	
+
 	public static long TIMESTAMP = 0;
 	public static int POSTGRES_NUM_OPS_METRIC_COUNT = 0;
-	
+
 	public static final String JDBC_DRIVER = "org.postgresql.Driver";  
 	public static final String DB_URL = "jdbc:postgresql://35.196.17.143:5432/opsmx";//172.9.239.1xx
-	
+
 	public static final String USER = "postgres";
 	public static final String PASS = "networks123";
 	public static String testData = "latency";
-	 
+
 
 	@RequestMapping("/greeting")
 	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		LOG.debug("BEGIN: greeting");  
-		System.out.println("Phani git trigger 1");
-		LOG.error("Encountered java.lang.InterruptedException while reading the images in the method : /greeting");
-		LOG.error("Unable to find properties file : java.io.FileNotFoundException at void hello line number ");
+		//LOG.error("Encountered java.lang.InterruptedException while reading the images in the method : /greeting");
+		//LOG.error("Unable to find properties file : java.io.FileNotFoundException at void hello line number ");
 
-	// ***** memory leakdemonstrating  *****//
+		// ***** memory leakdemonstrating  *****//
 		/*if (MEMORY_LEAK_TEST_STRING == null || MEMORY_LEAK_TEST_STRING.length() == 0) {
 			for (int i = 0; i < 10000; i++) {
 				String test = "" + i + "" + i + "" + i;
@@ -81,29 +80,29 @@ public class GreetingController {
 		}
 		BadKey badKey = new BadKey("");
 		badKey = new BadKey("" + (new Date().getTime()));
-	    
+
 		    //Commented the following line, causes problems with mem util 
 	        //BAD_KEY_MAP.put(badKey, "" + (new Date().getTime()) + "-" + (new Date().getTime()) + MEMORY_LEAK_TEST_STRING);		  
-	       */
+		 */
 		//***End of Memory leak code ***//
-		
-	   //Demonstrating Architectural Regression/*	POSTGRES_NUM_OPS_METRIC_COUNT += 1;
-           //Architectural Regression implementation has been shifted to ArchRegress.java, here only calling thread class.
-	  // test code
-         
-	   //ArchRegress test = new ArchRegress();
-	      // //Thread th = new Thread(test);
-		
-	    try{
-		Thread th = new Thread();
-		//th.sleep(40);
-		LOG.info("Greeting-Latency ");
-		//th.start();
-            }catch(Exception ex) {
-		LOG.error("Error: ", ex);
-	     }	
-           // **** End of architectural regression code ***//
-	       	
+
+		//Demonstrating Architectural Regression/*	POSTGRES_NUM_OPS_METRIC_COUNT += 1;
+		//Architectural Regression implementation has been shifted to ArchRegress.java, here only calling thread class.
+		// test code
+
+		//ArchRegress test = new ArchRegress();
+		// //Thread th = new Thread(test);
+
+		try{
+			Thread th = new Thread();
+			//th.sleep(40);
+			//LOG.info("Greeting-Latency ");
+			//th.start();
+		}catch(Exception ex) {
+			LOG.error("Error: ", ex);
+		}	
+		// **** End of architectural regression code ***//
+
 		/*if (Long.compare(TIMESTAMP, 0l) == 0
 				|| Long.compare(((new Date()).getTime() - TIMESTAMP), 5000) >= 0) {
 			TIMESTAMP = (new Date()).getTime();
@@ -125,35 +124,35 @@ public class GreetingController {
 		}*/
 		LOG.info("Hello spark :: Inside greeting API");
 		//System.out.println("Hello spark + Inside greeting API");
-		
+
 		// System.out.println("HashMap size : "+ leakMap.size());
-       	       // new Greeting(MetricsRegistryClient.client().incrRequestCount(),String.format(template, name))
+		// new Greeting(MetricsRegistryClient.client().incrRequestCount(),String.format(template, name))
 		//return "HashMap size  : "+ BAD_KEY_MAP.size() + "\n String length  : " + MEMORY_LEAK_TEST_STRING.length() +"\n ";
 		String query;
 		StringBuffer sb = new StringBuffer();
 		String query2;
 		StringBuffer sb2 = new StringBuffer();
-		
+
 		ClassLoader cl = GreetingController.class.getClassLoader();
 		try{
 			BufferedReader br = new BufferedReader(new InputStreamReader(cl.getResource("testpage.html").openStream()));
 			while((query=br.readLine())!=null)
 				sb.append(query);	
 			br.close();
-			
-			  String whichdog = "dog1";
-			
+
+			String whichdog = "dog1";
+
 			/* Introducing Arch. regression*/
-			           
-			        // whichdog = "dog2";
+
+			// whichdog = "dog2";
 			/*till here*/
-			
+
 			BufferedReader br2 = new BufferedReader(new InputStreamReader(cl.getResource(whichdog).openStream()));
 			while((query2=br2.readLine())!=null)
 				sb2.append(query2);	
 			br2.close();
-			
-			
+
+
 		}
 		catch(Exception e){ 
 			e.printStackTrace();
@@ -163,7 +162,7 @@ public class GreetingController {
 		String greetResponse = sb.length()>0 ? sb.toString().replace("##dogimage##", sb2.toString()) : " No page found";
 		//LOG.debug("END: greeting" + "\n" + greetResponse);
 		return greetResponse;
-            // 	return "POSTGRES_NUM_OPS_METRIC_COUNT : "+ POSTGRES_NUM_OPS_METRIC_COUNT;
+		// 	return "POSTGRES_NUM_OPS_METRIC_COUNT : "+ POSTGRES_NUM_OPS_METRIC_COUNT;
 	}
 
 
@@ -188,24 +187,38 @@ public class GreetingController {
 		         System.exit(0);
 		       }
 		       System.out.println("Operation done successfully");
-		  
+
 	  }*/
-	
+
 	@RequestMapping("/dogcount")
 	public String dogCount() {
 		LOG.debug("BEGIN: dogCount");
 		try{
-		  Thread dh = new Thread();
-		  //dh.sleep(40);
-		  //LOG.info("dogcount-Latency ");
-                  }catch(Exception ex) {
-		LOG.error("Error: ", ex);
-	     }	
+			Thread dh = new Thread();
+			dh.sleep(30);
+			writeException();
+			//LOG.info("dogcount-Latency ");
+		}catch(Exception ex) {
+			LOG.error("Error: ", ex);
+		}	
 		String response = "{ \"dogCount\": 0.2 }";
 		LOG.debug("END: dogCount" + "\n" + response);
-	      return response;
+		return response;
 	}
-	
+
+	public void writeException() {
+		try
+		{ 
+			throw new NullPointerException("Animals count: null"); 
+		} 
+		catch(NullPointerException e) 
+		{ 
+			LOG.error("Encountered java.lang.InterruptedException while reading the images {}" , e.getStackTrace());
+			LOG.error("Unable to find properties file : java.io.FileNotFoundException at void hello line number: {}", e.getStackTrace());
+			LOG.error("ERROR ",e);
+		} 
+	}
+
 	@RequestMapping("/readfile")
 	public String readFile(@RequestParam("id") String id, @RequestParam("version") String version, @RequestParam("filename") String fileName) {
 		try {
@@ -218,14 +231,14 @@ public class GreetingController {
 			} catch (IOException e) {
 				return e.getMessage();
 			}
-			
-			
+
+
 		}catch(Exception ee) {
 			return "fail";
 		}
 		return "success";
 	}
-	
+
 	@RequestMapping("/writeLogs")
 	public String writeLogs(@RequestParam("filename") String fileName) {
 		String file = "/opt/" + fileName;
@@ -256,17 +269,18 @@ public class GreetingController {
 
 	@RequestMapping("/catcount")
 	public String catCount() {
-		LOG.debug("BEGIN: catCount");
+		LOG.info("BEGIN: catCount");
 		String result="{ \"catCount\": 0.1 }";
-		
+
 		try{
-		  Thread ch = new Thread();
-		  //ch.sleep(35);
-		 // LOG.info("catcount-Latency ");
-                  }catch(Exception ex) {
-		  LOG.error("Error: ", ex);
+			Thread ch = new Thread();
+			ch.sleep(30);
+			throw new Exception(); 
+			// LOG.info("catcount-Latency ");
+		}catch(Exception ex) {
+			LOG.error("ERROR: ", ex);
 		}
-		
+
 		/*Code for Architectural Regression, prerequisite is to have restapp running on k8 pod */
 		/*try{
 			for (int i=0; i<=1000; i++)
@@ -276,37 +290,37 @@ public class GreetingController {
 				con.setRequestMethod("GET");
 				con.setDoOutput(true);
 				con.setInstanceFollowRedirects(false);
-				
+
 				System.out.println("con.getResponseCode() ::"+con.getResponseCode());
 				System.out.println(con.getResponseMessage());
-				
+
 				BufferedReader br2 = new BufferedReader(new InputStreamReader(con.getInputStream()));
 				String response2; StringBuffer res2 = new StringBuffer();
 				while ((response2=br2.readLine())!=null)
 					res2.append(response2);
-					
+
 				System.out.println(res2);
 				if (!res2.toString().isEmpty())
 					result=res2.toString();
 			}
-			
+
 		}catch(MalformedURLException mue){
 			mue.printStackTrace();
 		}catch(IOException io){
 			io.printStackTrace();
 		}
 		/* till here */
-		
+
 		LOG.debug("END: catCount" + "\n" + result);
-	      return result;
+		return result;
 	}
-	
+
 	@RequestMapping("/status")
 	public String getStatus() {
 		LOG.debug("BEGIN: getStatus");
 		String response = " @@@@@@ Welcome to  Strategy World  @@@@@@";
 		LOG.debug("END: getStatus" + "\n" + response);
-	      return response;
+		return response;
 	}
 
 	@RequestMapping(value = "/mbeans")
@@ -314,7 +328,7 @@ public class GreetingController {
 		LOG.debug("BEGIN: mbeans");
 		System.out.println("New changes in mbeans");
 		MBeanServer server = ManagementFactory.getPlatformMBeanServer();
-//		Set mbeans = server.queryNames(null, null);
+		//		Set mbeans = server.queryNames(null, null);
 		/*
 		 * for (Object mbean : mbeans) { WriteAttributes(server,
 		 * (ObjectName)mbean); }
@@ -351,7 +365,7 @@ public class GreetingController {
 		}
 		LOG.debug("END: mbeans");
 	}
-	
+
 	//@RequestMapping("/configuration")
 	@RequestMapping(value="/configuration", produces = "text/plain")
 	public String configuration() throws IOException {
@@ -372,9 +386,9 @@ public class GreetingController {
 		LOG.debug("END: configuration");
 		return content.toString();
 	}
-	
-	
-	
+
+
+
 	private void writeIntoKairosDB(PrintWriter out, String metricName,String value) {
 
 		if (metricName != null) {
@@ -383,8 +397,8 @@ public class GreetingController {
 			System.out.println("The metric name and the key is : " + metricName
 					+ ", " + value);
 			if (value == null) {
-			   LOG.warn("Skipping write to Kairos as the response has no data for '{}' url",metricName);
-			   return;
+				LOG.warn("Skipping write to Kairos as the response has no data for '{}' url",metricName);
+				return;
 			}
 
 			StringBuilder putCommand = new StringBuilder();
@@ -393,19 +407,19 @@ public class GreetingController {
 			putCommand.append(metricName + " ");
 			putCommand.append(timeStamp + " ");
 			putCommand.append(value + " ");
-			
+
 			String hostname = "Unknown";
 			try
 			{
-			    InetAddress addr;
-			    addr = InetAddress.getLocalHost();
-			    hostname = addr.getHostName();
+				InetAddress addr;
+				addr = InetAddress.getLocalHost();
+				hostname = addr.getHostName();
 			}
 			catch (UnknownHostException ex)
 			{
-			    System.out.println("Hostname cannot be resolved.");
+				System.out.println("Hostname cannot be resolved.");
 			}
-			
+
 			putCommand.append("host="+hostname+" \n");
 
 			// LOG.info("Writing to KairosDb : {}", putCommand.toString());
@@ -417,17 +431,17 @@ public class GreetingController {
 		out.close();
 	}
 
-//	private void WriteAttributes(final MBeanServer mBeanServer,
-//			final ObjectName http) throws InstanceNotFoundException,
-//			IntrospectionException, ReflectionException {
-//		MBeanInfo info = mBeanServer.getMBeanInfo(http);
-//		MBeanAttributeInfo[] attrInfo = info.getAttributes();
-//
-//		System.out.println("Attributes for object: " + http + ":\n");
-//		for (MBeanAttributeInfo attr : attrInfo) {
-//			System.out.println("  " + attr.getName() + "\n");
-//		}
-//	}
+	//	private void WriteAttributes(final MBeanServer mBeanServer,
+	//			final ObjectName http) throws InstanceNotFoundException,
+	//			IntrospectionException, ReflectionException {
+	//		MBeanInfo info = mBeanServer.getMBeanInfo(http);
+	//		MBeanAttributeInfo[] attrInfo = info.getAttributes();
+	//
+	//		System.out.println("Attributes for object: " + http + ":\n");
+	//		for (MBeanAttributeInfo attr : attrInfo) {
+	//			System.out.println("  " + attr.getName() + "\n");
+	//		}
+	//	}
 
 	static class BadKey {
 		// no hashCode or equals();
